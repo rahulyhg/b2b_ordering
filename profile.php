17437d188b849a8a0b52350a490bd8a1ad55396e
Bruce Brophy <bruce.brophy519@gmail.com>
2017-01-15 15:04:46 -0500

    Added form inputs to profile.php

--- profile.php
+++ profile.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 require_once 'includes/global.inc.php';
 
@@ -11,16 +11,28 @@ if(!isset($_SESSION['logged_in'])) {
 $user = unserialize($_SESSION['user']);
 
 //initialize php variables used in the form
+$firstname = $user->firstname;
+$lastname = $user->lastname;
+$company = $user->company;
+$address = $user->address;
 $email = $user->email;
 $message = "";
 
 //check to see that the form has been submitted
-if(isset($_POST['submit-settings'])) { 
+if(isset($_POST['submit-settings'])) {
 
 	//retrieve the $_POST variables
+	$firstname = $_POST['firstname'];
+	$lastname = $_POST['lastname'];
+	$company = $_POST['company'];
+	$address = $_POST['address'];
 	$email = $_POST['email'];
 
 	$user->email = $email;
+	$user->firstname = $firstname;
+	$user->lastname = $lastname;
+	$user->company = $company;
+	$user->address = $address;
 	$user->save();
 
 	$message = "Settings Saved<br/>";
@@ -39,10 +51,16 @@ if(isset($_POST['submit-settings'])) {
 	<?php echo $message; ?>
 
 	<form action="profile.php" method="post">
-	
+
+	E-Mail: <input type="text" value="<?php echo $email; ?>" name="email" /><br/>
+	First Name: <input type="text" value="<?php echo $firstname; ?>" name="firstname" /><br/>
+	Last Name: <input type="text" value="<?php echo $lastname; ?>" name="lastname" /><br/>
+	Company: <input type="text" value="<?php echo $company; ?>" name="company" /><br/>
+	Address: <input type="text" value="<?php echo $address; ?>" name="address" /><br/>
 	E-Mail: <input type="text" value="<?php echo $email; ?>" name="email" /><br/>
 	<input type="submit" value="Update" name="submit-settings" />
-	
+
+
 	</form>
 </body>
 </html>
