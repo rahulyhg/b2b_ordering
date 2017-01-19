6265ac45c6da11bb7a65b4b91711c34ba0a1ee0b
Bruce Brophy <bruce.brophy519@gmail.com>
2017-01-18 21:48:15 -0500

    Added Header and Hero Markup to index.php & profile.php
    Styles for Header and Hero sections are completed and will need minimal changes
    Sign up and register links function and show the correct state if user is logged in or not

--- index.php
+++ index.php
@@ -6,21 +6,74 @@ require_once 'includes/global.inc.php';
 
 <!doctype html>
 <html>
-<head>
-    <title>Homepage</title>
+  <head>
+    <title>Home | Nike Business</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700,900" rel="stylesheet">
     <link rel="stylesheet" href="/css/styles.min.css">
-</head>
-<body>
+  </head>
+  <body>
+    <!-- HEADER -->
+    <header>
+      <div class="container">
+        <div class="row nav-container ">
+          <div class="nav-logo col-lg-2">
+            <img class="logo" src="/images/nike_swoosh.svg" alt="">
+          </div>
+          <!-- NAVIGATION -->
+          <ul class="header-nav col-lg-10">
+            <a href="index.php" class="nav-item"><li>orders</li></a>
+            <a href="#" class="nav-item"><li>products</li></a>
+            <a href="#" class="nav-item"><li>create order</li></a>
+            <a href="#" class="nav-item"><li>invoices</li></a>
+            <!-- Log In Form -->
+            <?php if(isset($_SESSION['logged_in'])) : ?>
+                <?php $user = unserialize($_SESSION['user']); ?>
+                <!-- <h1>Hello, <?php echo $user->username; ?>!</h1> -->
+                <a class="nav-item" href="profile.php"><li>Profile</li></a>
+                <a class="nav-item" href="logout.php"><li>Log Out</li></a>
+            <?php else : ?>
+                <!-- <h1>You are not logged in.</h1> -->
+                <a class="nav-item" href="login.php"><li>Log In</li></a>
+                <a class="nav-item" href="register-user.php"><li href="register-user.php">Register</li></a>
+            <?php endif; ?>
+          </ul>
+        </div>
+      </div>
+    </header>
 
-<?php if(isset($_SESSION['logged_in'])) : ?>
+    <!-- HERO SECTION -->
+    <section>
+      <div class="container">
+        <!-- HERO IMAGE -->
+        <div class="row">
+          <div class="hero-image order-image col-lg-12">
+          </div>
+        </div>
+        <!-- HERO BORDER -->
+        <div class="row">
+          <div class="hero-border col-lg-6 col-md-4"></div>
+        </div>
+        <!-- HERO HEADING -->
+        <div class="row">
+          <div class="hero-heading-container col-lg-6">
+            <h2 class="hero-heading">ORDERS</h2>
+          </div>
+        </div>
+      </div>
+      <!-- HERO BG BAR -->
+      <div class="hero-bar order-bar"></div>
+    </section>
+
+    <script src="js/index.min.js" charset="utf-8"></script>
+  </body>
+</html>
+
+<!-- Log In Form -->
+<!-- <?php if(isset($_SESSION['logged_in'])) : ?>
     <?php $user = unserialize($_SESSION['user']); ?>
     <h1>Hello, <?php echo $user->username; ?>!</h1>
     <a href="logout.php">Log Out</a> | <a href="profile.php">View Profile</a>
 <?php else : ?>
     <h1>You are not logged in.</h1>
     <a href="login.php">Log In</a> | <a href="register-user.php">Register</a>
-<?php endif; ?>
-
-<script src="js/index.min.js" charset="utf-8"></script>
-</body>
-</html>
+<?php endif; ?> -->
--- profile.php
+++ profile.php
@@ -44,15 +44,69 @@ if(isset($_POST['submit-settings'])) {
 
 <!doctype html>
 <html>
-<head>
-	<title>Profile</title>
-	<link rel="stylesheet" href="/css/styles.min.css">
-</head>
-<body>
-	<?php echo $message; ?>
+  <head>
+    <title>Home | Nike Business</title>
+    <link href="https://fonts.googleapis.com/css?family=Roboto:400,700,900" rel="stylesheet">
+    <link rel="stylesheet" href="/css/styles.min.css">
+  </head>
+  <body>
+		<!-- HEADER -->
+    <header>
+      <div class="container">
+        <div class="row nav-container ">
+          <div class="nav-logo col-lg-2">
+            <img class="logo" src="/images/nike_swoosh.svg" alt="">
+          </div>
+					<!-- NAVIGATION -->
+          <ul class="header-nav col-lg-10">
+            <a href="index.php" class="nav-item"><li>orders</li></a>
+            <a href="#" class="nav-item"><li>products</li></a>
+            <a href="#" class="nav-item"><li>create order</li></a>
+            <a href="#" class="nav-item"><li>invoices</li></a>
+            <!-- Log In Form -->
+            <?php if(isset($_SESSION['logged_in'])) : ?>
+                <?php $user = unserialize($_SESSION['user']); ?>
+                <!-- <h1>Hello, <?php echo $user->username; ?>!</h1> -->
+                <a class="nav-item" href="profile.php"><li>Profile</li></a>
+                <a class="nav-item" href="logout.php"><li>Log Out</li></a>
+            <?php else : ?>
+                <a class="nav-item" href="login.php"><li>Log In</li></a>
+                <a class="nav-item" href="register-user.php"><li href="register-user.php">Register</li></a>
+            <?php endif; ?>
+          </ul>
+        </div>
+      </div>
+    </header>
 
-	<form action="profile.php" method="post">
+		<!-- HERO SECTION -->
+    <section>
+      <div class="container">
+        <!-- HERO IMAGE -->
+        <div class="row">
+          <div class="hero-image profile-image col-lg-12">
+          </div>
+        </div>
+        <!-- HERO BORDER -->
+        <div class="row">
+          <div class="hero-border col-lg-6 col-md-4"></div>
+        </div>
+        <!-- HERO HEADING -->
+        <div class="row">
+          <div class="hero-heading-container col-lg-6">
+            <h2 class="hero-heading">PROFILE</h2>
+          </div>
+        </div>
+      </div>
+      <!-- HERO BG BAR -->
+      <div class="hero-bar profile-bar"></div>
+    </section>
 
+    <script src="js/index.min.js" charset="utf-8"></script>
+  </body>
+</html>
+
+	<!-- <?php echo $message; ?> -->
+	<!-- <form action="profile.php" method="post">
 	E-Mail: <input type="text" value="<?php echo $email; ?>" name="email" /><br/>
 	First Name: <input type="text" value="<?php echo $firstname; ?>" name="firstname" /><br/>
 	Last Name: <input type="text" value="<?php echo $lastname; ?>" name="lastname" /><br/>
@@ -60,8 +114,4 @@ if(isset($_POST['submit-settings'])) {
 	Address: <input type="text" value="<?php echo $address; ?>" name="address" /><br/>
 	E-Mail: <input type="text" value="<?php echo $email; ?>" name="email" /><br/>
 	<input type="submit" value="Update" name="submit-settings" />
-
-
-	</form>
-</body>
-</html>
+	</form> -->
