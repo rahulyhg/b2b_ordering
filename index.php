4ee344bc9227cb399342dce4c72786dc9a8d8f03
Bruce Brophy <bruce.brophy519@gmail.com>
2017-01-19 15:35:13 -0500

    Completed Index.php
    Added order cards and footer
    Removed tracking css from the project, will now only track scss
    Added social icons to images

--- index.php
+++ index.php
@@ -64,6 +64,118 @@ require_once 'includes/global.inc.php';
       <div class="hero-bar order-bar"></div>
     </section>
 
+    <!-- PAST ORDER SECTION-->
+    <section>
+      <!-- ORDER CARDS -->
+      <div class="container">
+        <!-- CARD ROW - STATIC ROW ONE -->
+        <div class="row">
+          <div class="card-ctn col-sm-3">
+            <div class="row">
+              <div class="card col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order total:</span>
+                  <span class="card-detail">$1 520.65 CAD</span>
+                </div>
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order placed:</span>
+                  <span class="card-detail">January 4, 2017</span>
+                </div>
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order number:</span>
+                  <span class="card-detail">709-1264864-509</span>
+                </div>
+                <div class="slide-up"></div>
+                <button class="btn card-btn">invoice</button>
+              </div>
+            </div>
+          </div>
+          <div class="card-ctn col-sm-3">
+            <div class="row">
+              <div class="card col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order total:</span>
+                  <span class="card-detail">$1 520.65 CAD</span>
+                </div>
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order placed:</span>
+                  <span class="card-detail">January 4, 2017</span>
+                </div>
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order number:</span>
+                  <span class="card-detail">709-1264864-509</span>
+                </div>
+                <div class="card-details">
+                  <button class="btn card-btn">invoice</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-ctn col-sm-3">
+            <div class="row">
+              <div class="card col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order total:</span>
+                  <span class="card-detail">$1 520.65 CAD</span>
+                </div>
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order placed:</span>
+                  <span class="card-detail">January 4, 2017</span>
+                </div>
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order number:</span>
+                  <span class="card-detail">709-1264864-509</span>
+                </div>
+                <div class="card-details">
+                  <button class="btn card-btn">invoice</button>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="card-ctn col-sm-3">
+            <div class="row">
+              <div class="card col-lg-8 col-lg-offset-2 col-md-10 col-md-offset-1">
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order total:</span>
+                  <span class="card-detail">$1 520.65 CAD</span>
+                </div>
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order placed:</span>
+                  <span class="card-detail">January 4, 2017</span>
+                </div>
+                <div class="card-details col-lg-12">
+                  <span class="card-label">order number:</span>
+                  <span class="card-detail">709-1264864-509</span>
+                </div>
+                <div class="card-details">
+                  <button class="btn card-btn">invoice</button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-5 copyright-text">
+            <p>All content from Nike.com - Used for Educational purposes</p>
+          </div>
+          <div class="col-md-2 footer-logo-ctn">
+            <img class="footer-logo" src="/images/white_nike_swoosh.svg" alt="">
+          </div>
+          <div class="col-md-5 footer-social">
+            <img class="footer-social-icon" src="/images/instagram.svg" alt="">
+            <img class="footer-social-icon" src="/images/twitter.svg" alt="">
+            <img class="footer-social-icon" src="/images/facebook.svg" alt="">
+          </div>
+        </div>
+      </div>
+    </footer>
+
     <script src="js/index.min.js" charset="utf-8"></script>
   </body>
 </html>
