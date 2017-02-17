6d299b4c2714a299bd87365cd03104d95c62e543
Bruce Brophy <bruce.brophy519@gmail.com>
2017-02-17 10:05:39 -0500

    Added Rough Add to cart feature

---
+++ cart.php
@@ -0,0 +1,37 @@
+<?php
+
+require_once 'includes/global.inc.php';
+
+$cart = new Cart();
+
+if(isset($_POST['empty-cart'])) {
+  $cart->emptyCart();
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <?php if (isset($_SESSION['cart'])) :
+      foreach ($_SESSION['cart'] as $item) :
+        $name = $item['product']->name;
+        $quantity = $item['quantity']; ?>
+
+        <?= $name ?>
+        <?= $quantity; ?>
+        <?php //var_dump($item); ?>
+
+    <?php endforeach; else: ?>
+      <?= "There seems to be no products in your cart";  ?>
+    <?php endif; ?>
+
+    <form class="" action="cart.php" method="post">
+      <input type="submit" name="empty-cart" value="Empty Cart">
+    </form>
+  </body>
+</html>
